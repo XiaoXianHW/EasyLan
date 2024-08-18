a1a940cabcf66d849b73062367bdbeccea4e5dcb
Darf <xiaoxian@axtn.net>
2024-08-18 19:59:45 +0800

    Update v1.5 [1.19.2]

--- src/main/java/org/xiaoxian/lan/ServerStopping.java
+++ src/main/java/org/xiaoxian/lan/ServerStopping.java
@@ -2,15 +2,14 @@ package org.xiaoxian.lan;
 
 import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import org.xiaoxian.EasyLAN;
 
 public class ServerStopping {
 
     @SubscribeEvent
     public void onServerStopping(ServerStoppingEvent event)
     {
-        if (event.getServer().isSingleplayer() && EasyLAN.HttpAPI) {
-            ShareToLan.StopHttpAPIServer();
+        if (event.getServer().isSingleplayer()) {
+            new ShareToLan().handleStop();
         }
     }
 }
